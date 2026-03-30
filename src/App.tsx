1670aa1e22abe43141148fec2f983359884d8191
Mathew Tomilson <147887812+mtomilson@users.noreply.github.com>
2026-03-29 23:09:02 -0400

    feat [SCRUM-125] [SCRUM-126]: login page, authentication, auth context/provider, routers (#2)
    * feat: login page, authentication, auth context/provider, routers
    
    * chore: additional comments
    
    * fix: fixed login page only displaying on the /login route, now displayed on home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
+import Login from "./pages/login";
 
 function App() {
- 
-
+  // If adding more routes follow login format <Route path="/pathname" element={<page component/>}/>
+  // all components inside authprovider component will have access to user data and session token
   return (
-    <>
-    <div className="">
-      test
-    </div>
-    </>
-  )
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<Login />} /> 
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,52 @@
+import { createContext, useContext, useState } from "react";
+
+type AuthContextType = {
+  user: any | null; // profile data from your profiles table
+  token: string | null;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => void;
+};
+
+const AuthContext = createContext<AuthContextType | null>(null); //auth context allows all children to use the data from this file
+                                                                 // important for authentication purposes
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState<string | null>(null);
+
+  async function login(email: string, password: string) {
+    const res = await fetch("http://localhost:3000/api/login", { // calls api and returns token and user data
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    });
+    const data = await res.json();
+    if (!res.ok) {
+      console.log("error: ", data.error);
+    } else {
+      console.log(data.user);
+      console.log(data.token);
+      setUser(data.user);
+      setToken(data.token);
+    }
+  }
+
+  function logout() {
+    setUser(null);
+    setToken(null);
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, token, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+// useAuth function is able to be used in any children component as long as it's wrapped in
+// the <AuthProvider> tags. It gives access to login, logout, user and token.
+export function useAuth() {
+  const context = useContext(AuthContext);
+  if (!context) throw new Error("useAuth must be used within AuthProvider");
+  return context;
+}
