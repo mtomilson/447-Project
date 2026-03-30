3415522ed448f1bacad6f28b01b67acca7835520
mtomilson <mtomilson51@gmail.com>
2026-03-29 23:06:01 -0400

    fix: fixed login page only displaying on the /login route, now displayed on home page

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />} /> 
+          <Route path="" element={<Login />} /> 
         </Routes>
       </BrowserRouter>
     </AuthProvider>
